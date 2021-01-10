2d69a19c12a20f3539ec487d6ce0c207c4f4806a
autotaker <autotaker@gmail.com>
2021-01-10 21:12:35 +0900

    tdd-green: fool implementation

--- src/Auth.hs
+++ src/Auth.hs
@@ -2,7 +2,7 @@ module Auth where
 
 import Lens.Micro.Platform (Lens', makeLenses)
 import RIO (RIO)
-import User (Password, User, Username)
+import User (Password, User (User), Username)
 
 data UserRepository env = UserRepository
   { _findByUsername :: Username -> RIO env (Maybe User),
@@ -15,7 +15,7 @@ class HasUserRepository env where
   userRepositoryL :: Lens' env (UserRepository env)
 
 signin :: (HasUserRepository env) => Username -> Password -> RIO env (Maybe User)
-signin = error "Let's implement"
+signin usernm passwd = pure $ Just (User usernm passwd)
 
 signup :: (HasUserRepository env) => Username -> Password -> RIO env (Maybe User)
 signup = error "Let's implement"
